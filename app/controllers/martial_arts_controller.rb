2e4a39c955514c40938d17051a1a9be8b92c339c
Terry <tezorobert@hotmail.com>
2015-01-07 18:42:03 +0000

    added martial_arts index

--- app/controllers/martial_arts_controller.rb
+++ app/controllers/martial_arts_controller.rb
@@ -1,6 +1,41 @@
 class MartialArtsController < ApplicationController
 	before_action :authenticate_user!, only: [ :new, :create ]
-	
+
   def index
+  	@martial_arts = Martial_art.order ('id DESC').all
+  end
+
+
+  def search
+	@search = params[:query]
+	@martial_arts = Movie.search(@search)
+	@no_results = !@martial_art
+  end
+
+  def new
+  	@martial_arts = Martial_art.new 
+  	@martial_arts = Martial_art.order ('id DESC').all
+  end
+
+  def create
+	@martial_art = Movie.new(parameters)
+	if @martial_art.valid?
+		@martial_art.save
+		flash[:info] = 'Great!!! Martial Art added'
+		return redirect_to root_path
   end
 end
+
+flash[:error] = "You have errors"
+
+private
+  def parameters
+    params.require(:martial_art).permit(:name, :popularity)
+  end
+end
+
+
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
+
+
+has_many :votes
+has_many :martial_arts {':through =>  '}
