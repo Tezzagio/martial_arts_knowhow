4e8adb101de6c4e1b660dbe6fea56d6d62be69d4
Terry <tezorobert@hotmail.com>
2015-01-08 19:35:12 +0000

    removed config line

--- app/controllers/martial_arts_controller.rb
+++ app/controllers/martial_arts_controller.rb
@@ -2,37 +2,40 @@ class MartialArtsController < ApplicationController
 	before_action :authenticate_user!, only: [ :new, :create ]
 
   def index
-  	 @martial_arts = Martial_art.order ('id DESC').all
+    @martial_arts = MartialArt.order('id DESC').all
   end
 
 
   def search
-	   @search = params[:query]
-	   @martial_arts = Movie.search(@search)
-	   @no_results = !@martial_art
+    @search = params[:query]
+    @martial_arts = Movie.search(@search)
+    @no_results = !@martial_art
   end
 
   def new
-  	 @martial_arts = Martial_art.new 
-  	 @martial_arts = Martial_art.order ('id DESC').all
+    @martial_arts = MartialArt.new 
+    @martial_arts = MartialArt.order('id DESC').all
   end
 
-  def create
-	@martial_art = Martial_art.new(parameters)
-	if @martial_art.valid?
-		 @martial_art.save
-		 flash[:info] = 'Great!!! Martial Art added'
-		 return redirect_to root_path
-  end
-end
-
+  def create_comment
+    @martial_art = MartialArt.new(parameters)
+    if @martial_art.valid?
+      @martial_art.save
+      flash[:info] = 'Great!!! Martial Art added'
+      return redirect_to root_path
+    end
     flash[:error] = "You have errors"
+  end
 
-private
+  private
+
   def parameters
     params.require(:martial_art).permit(:name, :popularity)
   end
+
 end
+
+
 
 
 
