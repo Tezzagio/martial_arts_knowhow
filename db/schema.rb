51c40c8d4938921ce60d07ab847ee798753f057a
Terry <tezorobert@hotmail.com>
2015-01-11 19:02:28 +0000

    latest commit

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20150110181036) do
 
 
   create_table "martial_arts", force: true do |t|
-    t.string   "name",       limit: nil
-    t.string   "description"
+    t.text   "name",       limit: nil
+    t.text   "description"
     t.integer  "user_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
@@ -56,16 +56,17 @@ ActiveRecord::Schema.define(version: 20150110181036) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
 
   create_table "widgets", force: true do |t|
     t.string   "name",       limit: nil
