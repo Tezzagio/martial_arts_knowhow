634e4735c0a1c7ff4bb4b40bcfa2995c14f2055e
Terry <tezorobert@hotmail.com>
2015-01-12 21:22:31 +0000

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112194002) do
+ActiveRecord::Schema.define(version: 20150112211006) do
+
+  create_table "blogs", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogs", ["category_id"], name: "index_blogs_on_category_id"
+  add_index "blogs", ["user_id"], name: "index_blogs_on_user_id"
 
   create_table "comments", force: true do |t|
     t.datetime "created_at"
