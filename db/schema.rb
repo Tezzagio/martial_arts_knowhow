7a55b05937d2ef822c7d87840d7c3dc17037a8e8
Terry <tezorobert@hotmail.com>
2015-01-12 15:03:13 +0000

    changed devise create_user.rb to create_user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,22 +14,15 @@
 ActiveRecord::Schema.define(version: 20150110181036) do
 
   create_table "comments", force: true do |t|
-    t.string   "body"
-    t.integer  "marial_art_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-    add_index "comments", ["martial_arts_id"], name: "index_comments_on_martial_arts_id"
-
-
   create_table "martial_arts", force: true do |t|
-    t.text     "name",       limit: nil
-    t.text     "description"
+    t.string   "name",       limit: nil
     t.integer  "user_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.string   "martial_arts_poster"
   end
 
   add_index "martial_arts", ["user_id"], name: "index_martial_arts_on_user_id"
@@ -40,41 +33,22 @@ ActiveRecord::Schema.define(version: 20150110181036) do
   end
 
   create_table "user_comments", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "comment_id"
-    t.integer  "upvotes"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "user_comments", ["comment_id"], name: "index_user_comments_on_comment_id"
-  add_index "user_comments", ["user_id"], name: "index_user_comments_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table "widgets", force: true do |t|
+    t.string   "name",       limit: nil
+    t.integer  "user_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
-  #create_table "widgets", force: true do |t|
-    #t.string   "name",       limit: nil
-    #t.integer  "user_id"
-    #t.datetime "created_at",             null: false
-    #t.datetime "updated_at",             null: false
-  #end
-
-  #add_index "widgets", ["user_id"], name: "index_widgets_on_user_id"
+  add_index "widgets", ["user_id"], name: "index_widgets_on_user_id"
 
 end
