7dc957d2b010eac949e299c4cdd4801b493c084c
Terry <tezorobert@hotmail.com>
2015-01-12 17:14:30 +0000

    modified views, db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110181036) do
+ActiveRecord::Schema.define(version: 20150112165236) do
 
   create_table "comments", force: true do |t|
     t.datetime "created_at"
@@ -40,7 +40,10 @@ ActiveRecord::Schema.define(version: 20150110181036) do
   create_table "users", force: true do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
   create_table "widgets", force: true do |t|
     t.string   "name",       limit: nil
