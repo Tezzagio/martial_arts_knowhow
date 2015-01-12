c302dc41f994793b76a9f8016f6f5bd6d75bb08f
Terry <tezorobert@hotmail.com>
2015-01-12 18:06:14 +0000

    removing usless migration files

--- db/migrate/20150112173149_remove_add_encrypted_password_to_users.rb
+++ db/migrate/20150112173149_remove_add_encrypted_password_to_users.rb
@@ -2,7 +2,7 @@ class RemoveAddEncryptedPasswordToUsers < ActiveRecord::Migration
   def change
     remove_column :users, :encrypted_password, :string
     remove_column :users, :default, :string
-    remove_column :users, :,, :string
+    remove_column :users, :string
     remove_column :users, :null, :string
     remove_column :users, :false, :string
   end
