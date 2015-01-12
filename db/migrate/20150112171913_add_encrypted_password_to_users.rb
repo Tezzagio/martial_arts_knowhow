e8d56f19648f9c5513b0c07cbd2aadbad3e89a7e
Terry <tezorobert@hotmail.com>
2015-01-12 17:44:14 +0000

    encrypted_password column

--- db/migrate/20150112171913_add_encrypted_password_to_users.rb
+++ db/migrate/20150112171913_add_encrypted_password_to_users.rb
@@ -2,7 +2,7 @@ class AddEncryptedPasswordToUsers < ActiveRecord::Migration
   def change
     add_column :users, :encrypted_password, :string
     add_column :users, :default, :string
-    add_column :users, :,, :string
+    add_column :users, :string
     add_column :users, :null, :string
     add_column :users, :false, :string
   end
