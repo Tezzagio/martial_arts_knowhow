1a7843f37bb716fc846f46f8cf4c1869d7e7572a
Terry <tezorobert@hotmail.com>
2015-01-12 18:42:22 +0000

    latest commit

---
+++ db/migrate/20150112184002_remove_encrypted_password_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveEncryptedPasswordFromUsers < ActiveRecord::Migration
+  def change
+    remove_index :users, :reset_password_token
+    remove_column :users, :reset_password_token, :string
+  end
+end
