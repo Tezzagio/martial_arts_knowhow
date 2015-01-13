611ff7d50b770389fcf025c64ae7dd526cedca78
Terry <tezorobert@hotmail.com>
2015-01-13 18:34:20 +0000

    ignore duplicate column

--- db/migrate/20150113163057_add_description_to_martial_arts.rb
+++ db/migrate/20150113163057_add_description_to_martial_arts.rb
@@ -1,5 +1,9 @@
 class AddDescriptionToMartialArts < ActiveRecord::Migration
   def change
+  begin
     add_column :martial_arts, :description, :text
+  rescue => err
+    # ignore..
+  end
   end
 end
