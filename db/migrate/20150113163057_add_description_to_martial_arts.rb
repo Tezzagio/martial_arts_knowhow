2a1adfe5645a519c67bad2b01f2ee386ca814fb4
Terry <tezorobert@hotmail.com>
2015-01-13 18:38:18 +0000

    remove redundant migration

--- db/migrate/20150113163057_add_description_to_martial_arts.rb
+++
@@ -1,9 +0,0 @@
-class AddDescriptionToMartialArts < ActiveRecord::Migration
-  def change
-  begin
-    add_column :martial_arts, :description, :text
-  rescue => err
-    # ignore..
-  end
-  end
-end
